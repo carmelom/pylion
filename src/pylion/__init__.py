17641f698285e56863144e84987d44c018cd2bcf
carmelom <carmelo.mordini@gmail.com>
2025-09-29 15:45:52 +0200

    Remove "import *" from __init__.py

--- src/pylion/__init__.py
+++ src/pylion/__init__.py
@@ -1,5 +1,50 @@
-from .functions import *
-from .pylion import Simulation, __version__
+from .functions import (
+    check_particles_in_domain,
+    compute,
+    createioncloud,
+    dump,
+    efield,
+    evolve,
+    ionneutralheating,
+    langevinbath,
+    lasercool,
+    linearpaultrap,
+    minimise,
+    placeions,
+    readdump,
+    squaresum,
+    thermalvelocities,
+    timeaverage,
+    trapaqtovoltage,
+)
+from .pylion import Attributes, Simulation, SimulationError, __version__
 
 __author__ = """Dimitris Trypogeorgos"""
 __email__ = "dtrypogiorgos@gmail.com"
+
+__all__ = [
+    # classes and exceptions
+    "Simulation",
+    "SimulationError",
+    "Attributes",
+    # version
+    "__version__",
+    # functions / utilities
+    "efield",
+    "placeions",
+    "createioncloud",
+    "evolve",
+    "thermalvelocities",
+    "minimise",
+    "ionneutralheating",
+    "langevinbath",
+    "lasercool",
+    "linearpaultrap",
+    "compute",
+    "timeaverage",
+    "squaresum",
+    "dump",
+    "trapaqtovoltage",
+    "check_particles_in_domain",
+    "readdump",
+]
